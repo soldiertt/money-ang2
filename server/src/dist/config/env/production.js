c8a128df5903ec89623d51fd86e35295ae109976
soldiertt <soldiertt@gmail.com>
2016-09-14 21:46:15 +0200

    update prod config

--- server/src/dist/config/env/production.js
+++ server/src/dist/config/env/production.js
@@ -1,6 +1,6 @@
 "use strict";
 var config_class_1 = require("./config.class");
-var config = new config_class_1.default("productionSessionSecret", "mongodb://admin:THVtih7CgW-a@" + process.env.OPENSHIFT_MONGODB_DB_HOST + ":" + process.env.OPENSHIFT_MONGODB_DB_PORT + "/moneyv2", "ejs");
+var config = new config_class_1.default("productionSessionSecret", "mongodb://localhost/moneyv2", "ejs");
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = config;
 //# sourceMappingURL=production.js.map
